694c711882d72c367f87d1813407ff2fe5828828
Alex Hrdy <alex@nsoc.club>
2022-10-25 09:47:53 -0500

    Update GrandPrix.ino
    basic working state

--- GrandPrix.ino
+++ GrandPrix.ino
@@ -5,12 +5,27 @@
 // RPM signal (falling) connects to digital 2
 /*****************************/
 // includes
-
+#include <SPI.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_HX8357.h"
+#include <util/atomic.h>
 /*****************************/
 // definitions
 #define pressurePin A3
 #define rpmPin 2
-
+#define TFT_CS 10
+#define TFT_DC 9
+#define TFT_RST -1 // RST can be set to -1 if you tie it to Arduino's reset
+Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
+// Color definitions
+#define BLACK    0x0000
+#define BLUE     0x001F
+#define RED      0xF800
+#define GREEN    0x07E0
+#define CYAN     0x07FF
+#define MAGENTA  0xF81F
+#define YELLOW   0xFFE0 
+#define WHITE    0xFFFF
 /*****************************/
 // variables
 unsigned int rpm,RPM;//varaible to store rpm
@@ -23,17 +38,36 @@ unsigned int oilPressure; //varible to store readable oil pressure
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);           //  setup serial
+  Serial.begin(115200);           //  setup serial
   //vss
   attachInterrupt(digitalPinToInterrupt(rpmPin),rpmCount,FALLING);//create interrupt on rpmPin to run rpmCount() on each pulse
   timeOld = millis();
+  //screen 
+  tft.begin();
+  // read diagnostics (optional but can help debug problems)
+  uint8_t x = tft.readcommand8(HX8357_RDPOWMODE);
+  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDMADCTL);
+  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDCOLMOD);
+  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDDIM);
+  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDDSDR);
+  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); 
+
+  tft.setRotation(3);
+  tft.fillScreen(HX8357_BLACK);
+
+  
   delay(2000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE){  
-    rpm = rpmCount();
+    rpm = readRpm();
   }
   //Serial.print("RPM:");
   //Serial.println(rpm);
@@ -49,7 +83,17 @@ void loop() {
     Serial.print("Oil Pressure:");
     Serial.println(oilPressure);
   }
-  delay(1000);
+
+  // screen
+  tft.fillRect(340, 0, 100, 60, BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(HX8357_WHITE);  tft.setTextSize(4);
+  tft.print("Oil Pressure:   ");
+  tft.println(oilPressure);
+  tft.print("Engine Speed:   ");
+  tft.println(rpm);  
+  
+  delay(200);
 }
 
 /*****************************/
